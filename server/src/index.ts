9a5e5fdde9c80aa02c50752ea05af7080cd2c383
LoyalEnv0y <cetin200012@gmail.com>
2023-08-22 13:01:06 +0300

    Integrated nodemon to watch for any changes with TS on the backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,12 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-	res.send('Welcome to the home page 🙋‍♂️');
+	res.send('<h1>Welcome Here</h1>');
 });
+
+app.get('/hi', (req, res) => {
+	res.send('<h1>Hi Mom <span>🙋‍♂️</span></h1>')
+})
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
