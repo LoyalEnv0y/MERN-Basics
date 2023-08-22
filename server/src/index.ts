f8245d9c24276a97d5deea9c3046dc31a579a73f
LoyalEnv0y <cetin200012@gmail.com>
2023-08-22 13:50:27 +0300

    Connected to Mongo Atlas and created the basic Decks model.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,32 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import Deck from './models/Deck';
 
 const app = express();
 const port = 5000;
+
+mongoose
+	.connect(
+		'mongodb+srv://LoyalEnv0y:RLvCtC1fUgRFfYfK@mern-main.br77pyu.mongodb.net/?retryWrites=true&w=majority'
+	)
+	.then(() => console.log('Mongo Atlas connection successful'))
+	.catch((err) =>
+		console.log('Error when connecting to Mongo Atlas. ERROR MESSAGE => ', err)
+	);
 
 app.get('/', (req, res) => {
 	res.send('<h1>Welcome Here</h1>');
 });
 
-app.get('/hi', (req, res) => {
-	res.send('<h1>Hi Mom <span>🙋‍♂️</span></h1>')
-})
+app.post('/decks', async (req, res) => {
+	const newDeck = new Deck({
+		title: 'Example Title',
+	});
+
+	const createdDeck = await newDeck.save();
+
+	res.json(createdDeck);
+});
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
---
+++ server/src/models/Deck.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const Deck = new Schema({
+	title: {
+		type: String,
+		require: true,
+	},
+});
+
+export default mongoose.model('Deck', Deck);
